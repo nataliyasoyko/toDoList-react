157bd8f4c5dfe638c632fd95ff97a0b5ea77c351
natalka <nataliya.soyko@gmail.com>
2020-07-16 21:14:44 +0200

    changed to react

---
+++ src/Tasks/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+
+const Tasks = (props) => (
+    <ul 
+    className="tasks__items">
+        {props.tasks.map(task => (
+            <li
+            key={task.id} className={`tasks__item${task.done && props.hideDoneTasks ? " tasks__item--hidden" : ""}`}
+            >
+            <button className="tasks__button tasks__button--done">
+            <i className={`fas fa-check${task.done ? "" : " fa-check--none" }`}></i>
+            </button>
+            <span className={`tasks__text${task.done ? " tasks__text--done" : ""}`}>{task.content}</span>
+            <button className="tasks__button tasks__button--delete">
+            <i className="far fa-trash-alt"></i>
+            </button>
+            </li>
+        ))}
+    </ul>
+        );
+
+export default Tasks;
