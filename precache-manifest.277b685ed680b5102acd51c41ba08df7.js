83733226c1182c628458b4557a0854461839cd85
natalka <nataliya.soyko@gmail.com>
2020-07-19 21:51:15 +0200

    Updates

---
+++ precache-manifest.277b685ed680b5102acd51c41ba08df7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36611817323fa817bd254a9506e6728a",
+    "url": "/toDoList-react/index.html"
+  },
+  {
+    "revision": "9aa7dae1765939962406",
+    "url": "/toDoList-react/static/css/main.63a8f6a5.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/toDoList-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDoList-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa7dae1765939962406",
+    "url": "/toDoList-react/static/js/main.b32698c6.chunk.js"
+  },
+  {
+    "revision": "940dd42d991a9e9236c8",
+    "url": "/toDoList-react/static/js/runtime-main.5e039b5f.js"
+  }
+]);
